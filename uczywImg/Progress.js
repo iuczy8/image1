3bb0347cb14a9fe4c55c03f646137d2b15d4570d
iuczy8 <121467400+iuczy8@users.noreply.github.com>
2023-04-09 10:50:14 +0800

    Upload by PicGo

---
+++ uczywImg/Progress.js
@@ -0,0 +1,18 @@
+$(function(){
+
+  var interval = setInterval(increment,30000);
+  var current = 0;
+
+  function increment(){
+    current++;
+    $('#counter').html(current+'%'); 
+    if(current == 100) { current = 0; }
+  }
+
+  $('.load-bar').mouseover(function(){
+            clearInterval(interval);
+  }).mouseout(function(){
+      interval = setInterval(increment,30000);
+         });
+
+});
